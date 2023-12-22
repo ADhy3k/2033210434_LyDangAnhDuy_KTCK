7fa441b33c50389a1539a36481ee16ad7bd496e2
Anh Duy <anhduykg3k@gmail.com>
2023-12-22 07:34:34 +0700

    First commit

---
+++ 2033210434_LyDangAnhDuy_KTCK/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2033210434_LyDangAnhDuy_KTCK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
